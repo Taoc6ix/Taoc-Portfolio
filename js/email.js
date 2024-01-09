6621878866b1fd115e40e8654fa4a9b3e44020de
taoc <sixzeri6@gmail.com>
2024-01-10 01:06:37 +0700

    add Form Message

---
+++ js/email.js
@@ -0,0 +1,21 @@
+function sendMessage(){
+    (function(){
+        emailjs.init("GlGUsqij0-v9MUSbM");
+    })();
+
+    var serviceID = "service_9eanoph";
+    var templateID = "template_rk2q5q6";
+
+    var params = {
+        sendername: document.querySelector("#name").value,
+        senderemail: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value
+};
+
+emailjs.send(serviceID, templateID, params)
+.then( res => {
+    alert('Terimakasih ' + params['sendername'] + '! Pesan Anda Telah Dikirim ke Rafli :)');
+})
+.catch();
+}
